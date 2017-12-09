c03a2e778985a8c45d979544d3d3bd0f745da368
Nav <navkiran.ghuman@gmail.com>
2017-12-09 14:46:29 -0500

    server and webpack
    server and webpack

---
+++ server.js
@@ -0,0 +1,19 @@
+"use strict"
+
+const express = require('express');
+const app = express();
+
+const path = require('path');
+
+// DEFINES A FOLDER FOR THE STATIC FILES
+app.use(express.static('public'));
+
+// DEFINES THE MAIN ENTRY POINT
+app.get('/', function(req, res){
+  res.sendFile(path.resolve(__dirname,
+    'public', 'index.html'))
+  });
+
+app.listen(3000, function(){
+  console.log('App web-server listening on port 3000');
+});
